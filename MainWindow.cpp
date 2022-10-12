775e6600360f71abdea70d41483bc21d64dcc781
Andrea <lollosositv@gmail.com>
2022-10-12 09:32:47 +0200

    part 2

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,34 +2,59 @@
 
 
 
-MainWindow::MainWindow(QGraphicsView* parent) : QGraphicsView(parent), scene(new QGraphicsScene(this)) {
+MainWindow::MainWindow(QGraphicsView* parent) : QMainWindow(parent) {
     QRect windowRect(0,0,this->width(), this->height());
-    
-    this->setScene(scene);
-   
+
+    scene = new QGraphicsScene(this);
+    view = new QGraphicsView(scene);
+    view->setCacheMode(QGraphicsView::CacheBackground);
+
     sceneRect.setX(0);
     sceneRect.setY(0);
     sceneRect.setWidth(this->width());
     sceneRect.setHeight(this->height());
     scene->setSceneRect(sceneRect);
     
-    this->setCacheMode(QGraphicsView::CacheBackground);
+    //view->show();
+
+    QHBoxLayout* vb = new QHBoxLayout();
+    vb->addItem(vb);
+    this->setLayout(vb);
+
+    
 
     show();
+
     scene->clear();
     
+    QPen p(Qt::blue);
+    scene->addRect(0,0,50,50, p, Qt::red);
     
 
-    QPainter painter(this);
+    QPainter painter(view);
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.fillRect(windowRect, Qt::yellow);
+  
 
-    QPen p(Qt::blue);
     
     
     painter.setPen(p);
-    painter.drawRect(0,0,100,100);
+    painter.drawRect(10,10,100,100);
+
+    painter.fillRect(windowRect, Qt::black);
+    painter.setViewport(0,0, sceneRect.width(), sceneRect.height());
+    
 
    // scene->clear();
    
     setWindowTitle(tr("Chip Example"));
 }
         
+
+void MainWindow::resizeEvent(QResizeEvent* event)
+{
+    QMainWindow::resizeEvent(event);
+    // Resize drawspace here
+    sceneRect.setWidth(this->width());
+    sceneRect.setHeight(this->height());
+}
--- MainWindow.h
+++ MainWindow.h
@@ -1,16 +1,28 @@
 #pragma once
 #include <QWindow>
+#include <QMainWindow>
 #include <QGraphicsView>
 #include <QPainter>
+#include <QHBoxLayout>
 
 
-class MainWindow : public QGraphicsView {
+class MainWindow : public QMainWindow {
     Q_OBJECT
+
+private:    
+    ~MainWindow() {};
 
 public:
     MainWindow(QGraphicsView* parent = 0);
 
+    void resizeEvent(QResizeEvent* event);
+
+    QGraphicsView* view;
     QGraphicsScene* scene;
     QRect sceneRect;
+
+    public slots:
+    
+
 };
 
