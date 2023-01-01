8a685a4993ccbc8cba76fc87657f6b89d9a3cb9c
Andrea <lollosositv@gmail.com>
2023-01-01 15:42:14 +0100

    Do not pass the flying status

--- Door.cpp
+++ Door.cpp
@@ -11,7 +11,7 @@ void Door::launchAction(Kirby* instanceForConsistency) {
 	Sounds::instance()->stopSound("Lobby");
 
 	if (level == std::string("levels/lobby")) {
-		Sounds::instance()->playSound("Kirby_Adventure_theme");
+		Sounds::instance()->playSound("Lobby");
 	}
 	if (level == std::string("levels/level1")) {
 		Sounds::instance()->playSound("Kirby_Adventure_theme");
@@ -23,14 +23,14 @@ void Door::launchAction(Kirby* instanceForConsistency) {
 		Sounds::instance()->playSound("Kirby_Adventure_theme");
 	}
 
-
 	if (!GameLoop::getInstance().loadGame(level, true, savecurrent)) {
 		GameLoop::getInstance().loadGame(level, false, false);
 	}
 
 	if (instanceForConsistency) {
 		Kirby* k = ((Kirby*)GameLoop::getInstance().KirbyInstance);
-		k->setAbility(instanceForConsistency->status);
+		if(instanceForConsistency->status != KIRBY_FLY)
+			k->setAbility(instanceForConsistency->status);
 		k->setHealth(instanceForConsistency->getHealth());
 	}
 	
